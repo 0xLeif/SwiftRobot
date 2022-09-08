501e39c72ae4e549e5b2a0a64576de9e501e1e8d
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2022-09-08 18:54:34 +0200

    Add SwiftRobotExample project

---
+++ Example/SwiftRobotExample/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftRobotExample
+//
+//  Created by Ahmed Shendy on 08/09/2022.
+//
+
+import SwiftUI
+import SwiftRobot
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Example/SwiftRobotExample/SwiftRobotExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRobotExampleApp.swift
+//  SwiftRobotExample
+//
+//  Created by Ahmed Shendy on 08/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRobotExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
